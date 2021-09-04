3cad58fcbd2e9f1fc0e5db9927a016c775f034c6
Michael Le <72768694+mileuc@users.noreply.github.com>
2021-09-03 18:43:23 -0600

    Update attempt.py

--- Exercise 1: Two Sum/attempt.py
+++ Exercise 1: Two Sum/attempt.py
@@ -1,27 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:    # has to return a list of integers
-        solution_array = []
-        # iterate through each number in the list and add that number to each of the other numbers in the list
-        for first_number in nums:
-            for second_number in nums:
-                first_number_index = nums.index(first_number)
-                second_number_index = nums.index(second_number)
-                # if indices don't match, add the numbers
-                if first_number_index != second_number_index:
-                    sum_num = first_number + second_number
-                    if sum_num == target:
-                        # if number equals target, add index of each number to solution array
-                        solution_array.append(first_number_index)
-                        solution_array.append(second_number_index)
-                        return solution_array
+        solution_indices = []
+        # iterate through each number in the list and add it to each number after it (starting with the last number in the list)
+        for first_number_index in range(len(nums) - 1):
+            second_number_index = len(nums) - 1     # reset second number to the last index each time
+            while second_number_index > first_number_index:
+                sum_value = nums[first_number_index] + nums[second_number_index]
+                if sum_value == target:
+                    solution_indices.append(first_number_index)
+                    solution_indices.append(second_number_index)
+                    return solution_indices
+                else:
+                    second_number_index -= 1
                           
-two_sum_finder = Solution()
-output = two_sum_finder.twoSum([2, 7, 11, 15], 9)
-print(output)
+# two_sum_finder = Solution()
+# output = two_sum_finder.twoSum([2, 7, 11, 15], 9)
+# print(output)
 
-
+# two_sum_finder = Solution()
 # output = two_sum_finder.twoSum([3, 2, 4], 6)
 # print(output)
+
+two_sum_finder = Solution()
+output = two_sum_finder.twoSum([3, 3], 6)
+print(output)
 
 
 # note to self: don't use the remove method, it removes the value from the initial array, even if you assign it to another variable beforehand
